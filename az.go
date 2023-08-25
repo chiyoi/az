f1b80fb5c5307a6b6d30fb5b43afc23574e30821
chiyoi <chiyoi2140@outlook.com>
2023-08-25 06:18:29 +0000

    update: add error handler IsConflict.

--- az.go
+++ az.go
@@ -11,3 +11,8 @@ func IsNotFound(err error) bool {
 	var re *azcore.ResponseError
 	return errors.As(err, &re) && re.StatusCode == http.StatusNotFound
 }
+
+func IsConflict(err error) bool {
+	var re *azcore.ResponseError
+	return errors.As(err, &re) && re.StatusCode == http.StatusConflict
+}
