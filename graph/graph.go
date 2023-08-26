3dd76d816f00af612b09c88bae446a1d0cfb1f7a
chiyoi <chiyoi2140@outlook.com>
2023-08-26 05:59:57 +0000

    update: graph Add Request. Add Endpoint.

--- graph/graph.go
+++ graph/graph.go
@@ -2,29 +2,38 @@ package graph
 
 import (
 	"context"
+	"io"
 	"net/http"
 	"net/url"
 
 	"github.com/chiyoi/apricot/kitsune"
 )
 
-func Read(ctx context.Context, endpoint string, token string, v any, query ...string) (err error) {
-	u, err := url.JoinPath(endpoint, query...)
-	if err != nil {
-		return
-	}
+const (
+	Endpoint = "https://graph.microsoft.com/"
+)
 
-	r, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
-	if err != nil {
-		return
-	}
-	kitsune.SetAuthorization(r.Header, token)
-
-	re, err := http.DefaultClient.Do(r)
+func Read(ctx context.Context, token string, v any, query ...string) (err error) {
+	re, err := Request(ctx, http.MethodGet, nil, token, query...)
 	if err != nil {
 		return
 	}
 	defer re.Body.Close()
 
 	return kitsune.ParseResponse(re, v)
+}
+
+func Request(ctx context.Context, method string, body io.Reader, token string, query ...string) (re *http.Response, err error) {
+	u, err := url.JoinPath(Endpoint, query...)
+	if err != nil {
+		return
+	}
+
+	r, err := http.NewRequestWithContext(ctx, method, u, body)
+	if err != nil {
+		return
+	}
+	kitsune.SetAuthorization(r.Header, token)
+
+	return http.DefaultClient.Do(r)
 }
